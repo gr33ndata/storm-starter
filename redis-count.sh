842e9ffba9d2fcd43ea01d62dd9c5278f0977545
Caio SBA <caiosba@gmail.com>
2014-03-12 15:23:55 -0300

    Fixing redis_count.sh

--- redis-count.sh
+++ redis-count.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # Helper script to check if data was correcly retrieved
-n=$(redis-cli keys "*" | egrep -v 'ERR|empty|^$' | wc -l)
+ntweets=$(redis-cli keys "tweet:*" | egrep -v 'ERR|empty|^$' | wc -l)
 nlangs=$(redis-cli keys "lang:*" | egrep -v 'ERR|empty|^$' | wc -l)
-ntweets=$((n - nlangs))
 sum=0
 for lang in $(redis-cli keys "lang:*" | sed 's/.*"//g')
 do
   sum=$((sum + `redis-cli get $lang | sed 's/"//g'`))
 done
-echo "There are $n records on the Redis database, from which $nlangs are languages and $ntweets are tweets, and the sum of all languages is $sum."
+echo "There are $ntweets tweets and $nlangs languages on the Redis database, and the sum of all languages is $sum."
