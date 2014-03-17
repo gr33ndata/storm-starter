8cc0b1f0b47c2662182e3e9b2ca423d4f0e4d633
Tarek Amr <gr33ndata@yahoo.com>
2014-03-17 23:16:55 +0200

    .

---
+++ getlanguage-dysl.py
@@ -0,0 +1,14 @@
+import storm
+#import langid
+from dysl.langid import LangID
+
+class GetLanguageDyslBolt(storm.BasicBolt):
+  def process(self, tup):
+    text = tup.values[1]
+    #language = langid.classify(text)[0]
+    l = LangID()
+    l.train()
+    language = l.classify(text)
+    storm.emit([tup.values[0], language])
+
+GetLanguageDyslBolt().run()
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn -f m2-pom.xml compile exec:java -Dexec.classpathScope=compile -Dstorm.topology=storm.starter.TweetsLanguages
+mvn -e -f m2-pom.xml compile exec:java -Dexec.classpathScope=compile -Dstorm.topology=storm.starter.TweetsLanguages
