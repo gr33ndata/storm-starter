fb26bee215291a1dbce7ed93a29ba2acdf5c42a2
Tarek Amr <gr33ndata@yahoo.com>
2014-03-19 18:22:17 +0200

    For performace reasons, moved the learning phase to GetLanguageDyslBolt __init__(), so as process() does not need to re-learn each time it is called

--- multilang/resources/getlanguage-dysl.py
+++ multilang/resources/getlanguage-dysl.py
@@ -1,14 +1,22 @@
 import storm
-#import langid
+from storm import BasicBolt
 from dysl.langid import LangID
 
-class GetLanguageDyslBolt(storm.BasicBolt):
+class GetLanguageDyslBolt(BasicBolt):
+
+  def __init__(self, *args, **kwargs):
+    #super(BasicBolt, self).__init__(*args, **kwargs)
+    #print dir(BasicBolt)
+    #BasicBolt.__init__(*args, **kwargs)
+    self.l = LangID()
+    self.l.train()
+
   def process(self, tup):
     text = tup.values[1]
     #language = langid.classify(text)[0]
-    l = LangID()
-    l.train()
-    language = l.classify(text)
+    #l = LangID()
+    #l.train()
+    language = self.l.classify(text)
     storm.emit([tup.values[0], language])
 
 GetLanguageDyslBolt().run()
